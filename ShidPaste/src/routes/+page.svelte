09b58f93324f4753ffb9fd412064607b3c5d6b7a
Draxcodes <joelparkinson@live.co.uk>
2022-09-13 20:39:07 +0100

    Increase maximum line number

--- ShidPaste/src/routes/+page.svelte
+++ ShidPaste/src/routes/+page.svelte
@@ -6,6 +6,6 @@
 
 <div class="flex items-start space-x-4">
 	<div class="min-w-0 flex-1">
-		<TextArea bind:value={val} minRows={35} maxRows={40} />
+		<TextArea bind:value={val} minRows={35} maxRows={7000} />
 	</div>
 </div>
