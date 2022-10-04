f8c56216433052033a6d76db61337ce00ec62f0f
Draxcodes <joelparkinson@live.co.uk>
2022-10-04 13:21:33 +0100

    Add Discord Auth with SupaBase

--- ShidPaste/src/routes/account/+page.svelte
+++ ShidPaste/src/routes/account/+page.svelte
@@ -1,14 +1,22 @@
 <script>
+	import { user } from '$lib/sessionStore';
+	import { supabase } from '$lib/supabaseClient';
+	import Auth from '../../components/Auth.svelte';
+	import Profile from '../../components/Profile.svelte';
 
-	let email;
-	let password;
+	user.set(supabase.auth.user());
 
-	const handleSubmit = async () => {
-		
-	};
-
-	const handleLogout = async () => {
-		
-	};
+	supabase.auth.onAuthStateChange((_, session) => {
+		if (session) {
+			user.set(session.user);
+		} else {
+			user.set(null);
+		}
+	});
 </script>
 
+{#if $user}
+	<Profile />
+{:else}
+	<Auth />
+{/if}
